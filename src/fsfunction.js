91194246025fc607141d929697a255900b462927
Andrea Flores <118763091+AndreaFlo1794@users.noreply.github.com>
2023-03-21 17:29:41 -0500

    avances funciones para MDLINKS

--- src/fsfunction.js
+++ src/fsfunction.js
@@ -1,37 +1,100 @@
 const fs = require('fs');
 const path = require('path');
+const marked = require('marked');
 
 //.......Identificar si la ruta existe - retorna true o false.......
 
-const routeExists = (route) =>  fs.existsSync(route) ;
-if(routeExists('src/API.js')){
-console.log('La ruta existe');
-}
+const routeExists = (route) => fs.existsSync(route);
 
 //.......Identificar si la ruta es absoluta - retorna true o false.......
 
 const absolutePath = (pathRoute) => path.isAbsolute(pathRoute);
-if(absolutePath('C:\\Users\\ANDREA\\OneDrive\\Desktop\\Laboratoria\\DEV003-md-links\\README.md')){
-  console.log('La ruta es absoluta');
-  }
+
 //.......Si la ruta es relativa convertir a absoluta.......
 
-const routeRelative = (pathRoute) => {absolutePath(pathRoute) ? pathRoute : path.resolve(pathRoute)};
+const routeRelative = (pathRoute) => { absolutePath(pathRoute) ? pathRoute : path.resolve(pathRoute) };
 
 //.......Si la ruta es un directorio.......
 
-const directory = (route) => fs.statSync(route).isDirectory();
-
-//.......Para saber que tipo de archivo es.......
-
-const typeFile = (pathRoute) => path.extname(pathRoute);
+const ifItsAdirectory = (route) => fs.statSync(route).isDirectory();
 
 //.......Si es un directorio que lo lea.......
+
+const directory = (route) => fs.readdirSync(route);
+
+
+//.......Si la ruta es un archivo.......
+const ifItsAFile = (path) => fs.statSync(path).isFile();
+
+//.......Para saber si es un archivo MD.......
+
+const typeFile = (pathRoute) => path.extname(pathRoute) === '.md' ? true : false;
+
+
+//.......Si es un directorio lo recorra y si no consultar si es un archivo .md.......
+
+const getfilesArray = (routePath) => {
+  let filesArray = [];
+  absolutePath;
+
+  if (!ifItsAdirectory(route) && typeFile(pathRoute)) {
+    filesArray.push(routePath);
+
+  } else if (ifItsAdirectory(route)) {
+    directory(route).forEach((element) => {
+      const newPath = path.join(route, element);
+      const newArray = getfilesArray(path.resolve(newPath));
+      filesArray = filesArray.concat(newArray);
+    })
+  }
+  return filesArray;
+};
+
+//.......Leer archivo .md.......
+
+const fileRead = (routePath) => {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(routePath, 'utf8', (error, data) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(data)
+    })
+  })
+}
+
+//.......Para extraer los links.......
+const searchLinks = (route) => {
+  let arrayLinks = [];
+  const renderer = new marked.Renderer();
+  const ArrayGetMd = getfilesArray(route);
+  ArrayGetMd.forEach((file) => {
+    renderer.link = (href, title, text) => {
+      const objLinks = {
+        href,
+        text,
+        file,
+      };
+      //extration links
+      arrayLinks.push(objLinks);
+    };
+    marked(readFile(file), { renderer });
+  });
+  return arrayLinks;
+}
+
+
 module.exports = {
-    routeExists,
-    absolutePath,
-    routeRelative,
-    directory,
-    typeFile
-  };
-  
+  routeExists,
+  absolutePath,
+  routeRelative,
+  ifItsAdirectory,
+  ifItsAFile,
+  typeFile,
+  directory,
+  getfilesArray,
+  fileRead,
+  searchLinks,
+
+
+};
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,5 +1,5 @@
 const { routeExists, absolutePath,
-  routeRelative } = require('../src/fsfunction.js');
+  routeRelative, ifItsAdirectory, ifItsAFile, typeFile } = require('../src/fsfunction.js');
 
 //---Test para si existe la ruta
 describe('routeExists', () => {
@@ -28,8 +28,33 @@ describe('absolutePath', () => {
 describe('routeRelative', () => {
 
   it('should return an absolute route', () => {
-    const pathRoute = 'src/fsfunction.js'
+    const pathRoute = 'README.md'
     expect(routeRelative(pathRoute)).toBe('C:\\Users\\ANDREA\\OneDrive\\Desktop\\Laboratoria\\DEV003-md-links\\README.md');
   });
+});
 
+//---Test para saber si es un directorio
+describe('ifItsAdirectory', () => {
+  it('should return a directory .', () => {
+    const path = 'README.md'
+    expect(ifItsAdirectory(path)).toBe(false)
+  })
+});
+
+//---Test para saber si es un archivo
+describe('ifItsAFile', () => {
+  it('should return a directory .', () => {
+    const path = 'C:\\Users\\ANDREA\\OneDrive\\Desktop\\Laboratoria\\DEV003-md-links'
+    expect(ifItsAFile(path)).toBe(false)
+  })
+});
+
+// -------------Si debe retornar la extension del archivo --------
+
+describe('typeFile', () => {
+  it('should return a extension file .', () => {
+    const path = 'README.md'
+
+    expect(typeFile(path)).toBe(true)
+  })
 });
